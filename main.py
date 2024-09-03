e11639e178b15ed477549e2961cd64fbd431a2d5
Y2O-Dev <yakuboyusuf@gmail.com>
2024-09-03 20:13:26 +0100

    update path to standard posts

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    publish: bool = True
+    rating: Optional[int] = None
 
 # @=decorator
 @app.get("/")
 async def root():
     return {"message": "Hello World, I'm learning API"}
 
-@app.get("/post")
+@app.get("/posts")
 async def get_post():
     return {"This is your API request..."}
 
 # The first path operation that matches will run
 
-@app.post("/post")
+@app.post("/posts")
 def create_post(new_post: Post):    #extraction of data from the body 
-    print(new_post.title)
-    return{"new_post": "Data"}
+    print(new_post)
+    print(new_post.dict())
+    return{"Data": new_post}
 
 #return{"Create": "New post successfully"}
