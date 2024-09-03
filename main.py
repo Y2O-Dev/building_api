8100e2b81b6c3b648434b71b77b91288ca1fd03b
Y2O-Dev <yakuboyusuf@gmail.com>
2024-09-03 11:31:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+# @=decorator
+@app.get("/")
+async def root():
+    return {"message": "Hello World, I'm learning API"}
+
+@app.get("/post")
+async def get_post():
+    return {"This is your API request..."}
+
+# The first path operation that matches will run
+
+@app.post("/post")
+def create_post(new_post: Post):    #extraction of data from the body 
+    print(new_post)
+    return{"new_post": "Data"}
+
+#return{"Create": "New post successfully"}
